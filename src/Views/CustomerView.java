008b323dafe13d957461308a0c147a1f23fb7cd2
Sean O'Beirne <sean.t.obeirne@gmail.com>
2019-03-23 16:33:27 -0400

    renames view files

---
+++ src/Views/CustomerView.java
@@ -0,0 +1,16 @@
+package Views;
+
+/**
+ * View for a customer accessing the application to view things such as order history, account information, etc.
+ *
+ */
+
+public class Customer {
+
+
+
+
+
+
+
+}
